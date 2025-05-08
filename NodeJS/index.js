804456b8c1463760ce99dddf7a16abcbd76d1fc5
oSLaYN <rodrigo.6640@gmail.com>
2025-05-08 15:32:01 +0100

    Update Diretórios

--- NodeJS/index.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import routes from './api.js';
-const app = express();
-app.use(express.json());
-
-app.use(express.static('public', {extensions: ['js']}));
-app.use('/api/v1/', routes);
-
-app.listen(6500, () => { console.log('> Started at http://localhost:6500') })
