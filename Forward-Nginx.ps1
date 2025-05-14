767be8d2dbaa5cfb2454dcf88003adddf43dbc31
oSLaYN <rodrigo.6640@gmail.com>
2025-05-14 16:09:15 +0100

    Nginx Upload

---
+++ Forward-Nginx.ps1
@@ -0,0 +1,22 @@
+if (-not (Get-Command "kubectl" -ErrorAction SilentlyContinue)) {
+    Write-Host "kubectl não encontrado. Verifique se está instalado e no PATH."
+    exit
+}
+
+# Verifica se o Minikube está em execução
+$minikubeStatus = minikube status | Select-String -Pattern "host: Running"
+
+if (-not $minikubeStatus) {
+    Write-Host "`nMinikube não está em execução. Inicie o Minikube antes de rodar o script."
+    exit
+}
+
+$service = kubectl get service nginx -n nginx --no-headers -o custom-columns=":metadata.name" 2>$null
+
+if (-not $service) {
+    Write-Host "`n[ERRO] Serviço 'nginx' não encontrado. Verifique se o serviço está correto."
+    exit
+}
+
+Write-Host "`nIniciando port-forward de localhost:32500 -> nginx:80"
+kubectl port-forward service/nginx 32500:80 -n nginx
--- Start-Everything.ps1
+++ Start-Everything.ps1
@@ -6,6 +6,7 @@ minikube ip
 
 # Executa os scripts em segundo plano (processos separados)
 Start-Process -NoNewWindow -FilePath "powershell.exe" -ArgumentList "-File", "./Forward-Database.ps1"
+Start-Process -NoNewWindow -FilePath "powershell.exe" -ArgumentList "-File", "./Forward-Nginx.ps1"
 
 # Registra um evento de saída para quando o PowerShell for fechado
 $exitingEvent = Register-EngineEvent PowerShell.Exiting -Action {
