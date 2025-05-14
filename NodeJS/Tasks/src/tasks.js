64b534f10de1371cd23a4d367f4ef466a265499a
oSLaYN <rodrigo.6640@gmail.com>
2025-05-14 10:23:29 +0100

    Update Tasks Module API

--- NodeJS/Tasks/src/tasks.js
+++ NodeJS/Tasks/src/tasks.js
@@ -13,7 +13,7 @@ routes.get("/getTasks/:userid", async (req, res) => {
     if (!userid || userid==null) return res.status(404).send("ID de Utilizador Inválido.");
     try {
         const isAuthorized = await fetch('localhost:31000/api/v1/auth/isAuthorized');
-        if (!isAuthorized) { return res.status(400).send("Sessão Inválida, Sem Acesso. ")}
+        if (!isAuthorized) { return res.status(400).send("Sessão Inválida, Sem Acesso.")}
         try {
             const {err, results} = await sqlQuery("SELECT * FROM tasks WHERE userid = ?", [userid]);
             if (err) return res.status(500).send("Ocorreu um Erro: "+err.message);
@@ -28,17 +28,18 @@ routes.get("/getTasks/:userid", async (req, res) => {
 
 routes.post("/createTask/:userid", async (req, res) => {
     const userid = (req.params && req.params.userid ? req.params.userid : null)
-    const task = (req.query && req.query.task ? req.query.task : null);
+    const title = (req.query && req.query.title ? req.query.title : null)
+    const description = (req.query && req.query.description ? req.query.description : null)
     if (!userid || userid==null) return res.status(404).send("ID de Utilizador Inválido.");
-    if (!task || task==null) return res.status(404).send("Informação da Tarefa Inválida.");
+    if (!title || title == null || !description || description==null) return res.status(404).send("Informação da Tarefa Inválida.");
     try {
         const isAuthorized = await fetch('localhost:31000/api/v1/auth/isAuthorized');
-        if (!isAuthorized) { return res.status(400).send("Sessão Inválida, Sem Acesso. ")}
+        if (!isAuthorized) { return res.status(400).send("Sessão Inválida, Sem Acesso.")}
         try {
             const user = await fetch(`localhost:33000/api/v1/users/getUserByID?id=${userid}`);
             if (!user) { return res.status(404).send("Utilizador Inválido.") };
             try {
-                const {err, results} = await sqlQuery("INSERT INTO tasks (?, ?) VALUES (userid, task)", [userid, task]);
+                const {err, results} = await sqlQuery("INSERT INTO tasks (?, ?) VALUES (userid, title, description)", [userid, task]);
                 if (err) return res.status(500).send('Ocorreu um Erro:'+err.message);
                 return res.status(200).json(results);
             } catch (error) {
@@ -54,12 +55,13 @@ routes.post("/createTask/:userid", async (req, res) => {
 
 routes.post("/updateTask/:taskid", async (req, res) => { 
     const taskid = (req.params && req.params.taskid ? req.params.taskid : null)
+    const tasktitle = (req.query && req.query.tasktitle ? req.query.tasktitle : null)
     const taskinfo = (req.query && req.query.taskinfo ? req.query.taskinfo : null)
     const taskstatus = (req.query && req.query.taskstatus ? req.query.taskstatus : null)
-    if (!taskid || taskid == null || !taskinfo || taskinfo==null || !taskstatus || taskstatus == null) return res.status(404).send("Informação da Tarefa Inválida.");
+    if (!taskid || taskid == null || !tasktitle || tasktitle == null || !taskinfo || taskinfo==null || !taskstatus || taskstatus == null) return res.status(404).send("Informação da Tarefa Inválida.");
     try {
         const isAuthorized = await fetch('localhost:31000/api/v1/auth/isAuthorized');
-        if (!isAuthorized) { return res.status(400).send("Sessão Inválida, Sem Acesso. ")}
+        if (!isAuthorized) { return res.status(400).send("Sessão Inválida, Sem Acesso.")}
         try {
             const {err, results} = await sqlQuery("UPDATE tasks SET task=?, done=? WHERE id=?", [taskinfo, taskstatus, taskid]);
             if (err) return res.status(500).send('Ocorreu um Erro:'+err.message);
@@ -77,7 +79,7 @@ routes.delete("/deleteTask/:taskid", async (req, res) => {
     if (!taskid || taskid == null) return res.status(404).send("Informação da Tarefa Inválida.");
     try {
         const isAuthorized = await fetch('localhost:31000/api/v1/auth/isAuthorized');
-        if (!isAuthorized) { return res.status(400).send("Sessão Inválida, Sem Acesso. ")}
+        if (!isAuthorized) { return res.status(400).send("Sessão Inválida, Sem Acesso.")}
         try {
             const {err, results} = await sqlQuery("DELETE FROM tasks WHERE id=?", [taskid]);
             if (err) return res.status(500).send('Ocorreu um Erro:'+err.message);
