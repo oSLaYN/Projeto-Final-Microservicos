52939435a5e45b9fff5d48b5a5a4dfabb222da4d
oSLaYN <rodrigo.6640@gmail.com>
2025-05-29 22:40:48 +0100

    Update tasks.js

--- NodeJS/Tasks/src/tasks.js
+++ NodeJS/Tasks/src/tasks.js
@@ -13,20 +13,19 @@ routes.get("/getTasks", async (req, res) => {
         const userid = (req.query && req.query.userid ? req.query.userid : null)
         if (!userid || userid==null) return res.status(404).send("ID de Utilizador Inválido.");
         try {
-            const isAuthorized = await fetch('localhost:31000/api/v1/auth/isAuthorized');
-            if (!isAuthorized) { return res.status(400).send("Sessão Inválida, Sem Acesso.")}
+            const isAuthorized = await fetch('http://10.96.18.2/api/auth/isAuthorized');
+            if (!isAuthorized) { return res.status(400).json({message: "Sessão Inválida, Sem Acesso."})}
             try {
-                const {err, results} = await sqlQuery("SELECT * FROM tasks WHERE userid = ?", [userid]);
-                if (err) return res.status(500).send("Ocorreu um Erro: "+err.message);
+                const results = await sqlQuery("SELECT * FROM tasks WHERE userid = ?", [userid]);
                 return res.status(200).json(results);
             } catch (error) {
-                return res.status(500).send('Ocorreu um Erro: '+ error);
+                return res.status(500).json({message: 'Ocorreu um Erro: ', error})
             }
         } catch (error) {
-            return res.status(500).send('Ocorreu um Erro: '+ error);
+            return res.status(500).json({message: 'Ocorreu um Erro: ', error})
         }
     } catch (error) {
-        return res.status(500).send('Ocorreu um Erro: '+ error);
+        return res.status(500).json({message: 'Ocorreu um Erro: ', error})
     }
 });
 
@@ -34,26 +33,28 @@ routes.post("/createTask", async (req, res) => {
     try {
         const {userid, title, description} = req.body;
         try {
-            const isAuthorized = await fetch('localhost:31000/api/v1/auth/isAuthorized');
-            if (!isAuthorized) { return res.status(400).send("Sessão Inválida, Sem Acesso.")}
+            const isAuthorized = await fetch('http://10.96.18.2/api/auth/isAuthorized');
+            if (!isAuthorized) { return res.status(400).json({message: "Sessão Inválida, Sem Acesso."})}
             try {
-                const user = await fetch(`localhost:33000/api/v1/users/getUserByID?id=${userid}`);
-                if (!user) { return res.status(404).send("Utilizador Inválido.") };
-                try {
-                    const {err, results} = await sqlQuery("INSERT INTO tasks (?, ?, ?, ?) VALUES (userid, title, description, done)", [userid, title, description, 0]);
-                    if (err) return res.status(500).send('Ocorreu um Erro:'+err.message);
-                    return res.status(200).json(results);
-                } catch (error) {
-                    return res.status(500).send('Ocorreu um Erro:'+error);
+                const response = await fetch(`http://10.96.18.2/api/users/getUserByID?id=${userid}`);
+                const existingUsers = await response.json();
+                if (Array.isArray(existingUsers) && existingUsers.length > 0) {
+                    try {
+                        const results = await sqlQuery("INSERT INTO tasks (?, ?, ?, ?) VALUES (userid, title, description, done)", [userid, title, description, 0]);
+                        return res.status(200).json(results);
+                    } catch (error) {
+                        return res.status(500).json({message: 'Ocorreu um Erro: ', error});
+                    }
                 }
+                return res.status(404).json({message: "Utilizador Inválido."});
             } catch (error) {
-                return res.status(500).send('Ocorreu um Erro:'+error);
+                return res.status(500).json({message: 'Ocorreu um Erro: ', error});
             }
         } catch (error) {
-            return res.status(500).send('Ocorreu um Erro:'+error);
+            return res.status(500).json({message: 'Ocorreu um Erro: ', error});
         }
     } catch (error) {
-        return res.status(500).send('Ocorreu um Erro:'+error);
+        return res.status(500).json({message: 'Ocorreu um Erro: ', error});
     }
 });
 
@@ -61,20 +62,19 @@ routes.post("/updateTask", async (req, res) => {
     try { 
         const {taskid, tasktitle, taskinfo, taskstatus} = req.body;
         try {
-            const isAuthorized = await fetch('localhost:31000/api/v1/auth/isAuthorized');
-            if (!isAuthorized) { return res.status(400).send("Sessão Inválida, Sem Acesso.")}
+            const isAuthorized = await fetch('http://10.96.18.2/api/auth/isAuthorized');
+            if (!isAuthorized) { return res.status(400).json({message: "Sessão Inválida, Sem Acesso."})}
             try {
-                const {err, results} = await sqlQuery("UPDATE tasks SET title=?, description=?, done=? WHERE id=?", [tasktitle, taskinfo, taskstatus, taskid]);
-                if (err) return res.status(500).send('Ocorreu um Erro:'+err.message);
+                const results = await sqlQuery("UPDATE tasks SET title=?, description=?, done=? WHERE id=?", [tasktitle, taskinfo, taskstatus, taskid]);
                 return res.status(200).json(results);
             } catch (error) {
-                return res.status(500).send('Ocorreu um Erro:'+error);
+                return res.status(500).json({message: 'Ocorreu um Erro: ', error});
             }
         } catch (error) {
-            return res.status(500).send('Ocorreu um Erro:'+error);
+            return res.status(500).json({message: 'Ocorreu um Erro: ', error});
         }
     } catch (error) {
-        return res.status(500).send('Ocorreu um Erro:'+error);
+        return res.status(500).json({message: 'Ocorreu um Erro: ', error});
     }
 });
 
@@ -83,20 +83,19 @@ routes.delete("/deleteTask", async (req, res) => {
         const taskid = (req.query && req.query.taskid ? req.query.taskid : null)
         if (!taskid || taskid == null) return res.status(404).send("Informação da Tarefa Inválida.");
         try {
-            const isAuthorized = await fetch('localhost:31000/api/v1/auth/isAuthorized');
-            if (!isAuthorized) { return res.status(400).send("Sessão Inválida, Sem Acesso.")}
+            const isAuthorized = await fetch('http://10.96.18.2/api/auth/isAuthorized');
+            if (!isAuthorized) { return res.status(400).json({message: "Sessão Inválida, Sem Acesso."})}
             try {
-                const {err, results} = await sqlQuery("DELETE FROM tasks WHERE id=?", [taskid]);
-                if (err) return res.status(500).send('Ocorreu um Erro:'+err.message);
+                const results = await sqlQuery("DELETE FROM tasks WHERE id=?", [taskid]);
                 return res.status(200).json(results);
             } catch (error) {
-                return res.status(500).send('Ocorreu um Erro:'+error);
+                return res.status(500).json({message: 'Ocorreu um Erro: ', error});
             }
         } catch (error) {
-            return res.status(500).send('Ocorreu um Erro:'+error);
+            return res.status(500).json({message: 'Ocorreu um Erro: ', error});
         }
     } catch (error) {
-        return res.status(500).send('Ocorreu um Erro:'+error);
+        return res.status(500).json({message: 'Ocorreu um Erro: ', error});
     }
 });
 
